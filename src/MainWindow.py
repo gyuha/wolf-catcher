c11e4b40b5aa9eeb1c8e5eb83b1af61b4f09e7d7
gyuha <nicegyuha@gmail.com>
2022-08-16 09:05:34 +0900

    다운로드 흐름 생성

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -4,6 +4,7 @@ from PySide6.QtCore import Slot
 from ui.Ui_MainWindow import Ui_MainWindow
 from util.Clipboard import Clipboard
 from util.Config import Config
+from util.Downloader import Downloader
 
 from util.Site import Site
 from src.util.SeleniumWorker import SeleniumWorker
@@ -40,15 +41,30 @@ class MainWindow(QMainWindow):
     def add_clipboard(self, text: str, config: object):
         print('📢[MainWindow.py:31]: ', config)
         self.ui.statusbar.showMessage(text)
-    
+
     def get_button(self):
         if self.seleniumWorker.is_getting:
             return
 
-        self.seleniumWorker.get_with_retry(
-            "https://wfwf220.com/cv?toon=13766&num=1&title=%BA%F9%B0%CB%C0%C7%B8%B6%BC%FA%BB%E7%B0%A1%BC%BC%B0%E8%B8%A6%B4%D9%BD%BA%B8%B0%B4%D9%BC%BC%B0%E8%C3%D6%B0%AD%C0%C7%B8%B6%BC%FA%BB%E7%C0%CE%BC%D2%B3%E2%C0%BA%B8%B6%BC%FA%C7%D0%BF%F8%BF%A1%C0%D4%C7%D0%C7%D1%B4%D911%C8%AD",
-            "xpath",
-            "/html/body/section[1]/div[5]/img[1]"
-            )
-        
+        downloader = Downloader()
+        downloader.signals.download_state.connect(self.download_state)
+        downloader.add_files("./wfwf/카구야 님은 고백받고 싶어",
+                             [
+                                 "https://img8cloud.net/13991/ec7450ed_281_0.jpg",
+                                 "https://img8cloud.net/13991/ec7450ed_281_1.jpg",
+                                 "https://img8cloud.net/13991/ec7450ed_281_2.jpg",
+                                 "https://img8cloud.net/13991/ec7450ed_281_3.jpg"
+                             ],
+                             "https://wfwf220.com/"
+                             )
+        downloader.download_run()
+        # self.seleniumWorker.get_with_retry(
+        #     "https://wfwf220.com/cv?toon=13766&num=1&title=%BA%F9%B0%CB%C0%C7%B8%B6%BC%FA%BB%E7%B0%A1%BC%BC%B0%E8%B8%A6%B4%D9%BD%BA%B8%B0%B4%D9%BC%BC%B0%E8%C3%D6%B0%AD%C0%C7%B8%B6%BC%FA%BB%E7%C0%CE%BC%D2%B3%E2%C0%BA%B8%B6%BC%FA%C7%D0%BF%F8%BF%A1%C0%D4%C7%D0%C7%D1%B4%D911%C8%AD",
+        #     "xpath",
+        #     "/html/body/section[1]/div[5]/img[1]"
+        #     )
+    
+    @Slot(int)
+    def download_state(self, num):
+        print('📢[MainWindow.py:67] => complete', num)
 
--- src/util/Downloader.py
+++ src/util/Downloader.py
@@ -1,65 +1,112 @@
+import os
+import re
+import zipfile
 import requests
+from requests import Response
 import time
 from multiprocessing import cpu_count
 from multiprocessing.pool import ThreadPool
 from PySide6.QtCore import QObject, Signal, QThread
+from multiprocessing import Pool, cpu_count
+import pathlib
 
-class DownloaderSignal(QObject):
-    download_state = Signal(int)
-    download_error = Signal(str)
+from .file_name import strip_file_path
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36"
 }
 
-class Downloader(QThread):
+class DownloaderSignal(QObject):
+    download_state = Signal(int)
+    download_error = Signal(str)
+
+class Downloader:
     signals = DownloaderSignal()
 
     def __init__(self):
-        super()
-        files = []
+        self.files = []
+        self.downloading = False
 
 
-    def run(self):
-        if len(self.files):
-            self.signals.download_error.emit("empty list")
-            return
-        self.download_run()
-        # https://opensourceoptions.com/blog/use-python-to-download-multiple-files-or-urls-in-parallel/
-        
-
-    def add_files(self, path, urls):
-        pass        
+    def add_files(self, path: str, urls: list, referer):
+        for url in urls:
+            self.files.append([url, path, referer, len(self.files) + 1])
+        print('📢[Downloader.py:46]: ', self.files)
 
     
-    def download_run(self, args):
+    def download_run(self) -> None:
+        self.downloading = True
         cpus = cpu_count()
-        results = ThreadPool(cpus - 1).imap_unordered(self.download_url_to_file, args)
-        for result in results:
-            print('url:', result[0], 'time (s):', result[1])
+        pool = ThreadPool(cpus - 1)
+        try:
+            results = pool.imap_unordered(self.download_url_to_file, self.files)
+            # for result in results:
+            #     print('url:', result[0], 'time (s):', result[1])
+        finally:
+            pool.close()
+            pool.join()
+            self.downloading = False
+            self.signals.download_state.emit(len(self.files))
+            self.files.clear()
 
 
-    def download_url_to_file(self, args):
-        url, path, referer = args[0], args[1], args[2]
+
+    def download_url_to_file(self, args: list) -> None:
+        url, path, referer, number = args[0], args[1], args[2], args[3]
 
         headers = HEADERS
 
         if referer:
             headers["Referer"] = referer
         
+        requests.urllib3.disable_warnings()
+        session = requests.Session()
+        session.headers.update(headers)
+
         try:
-            requests.urllib3.disable_warnings()
-            response = requests.get(url, headers=headers)
-            self.__save_file(path, response)
+            response = session.get(url, stream=True, verify=False)
+            # response = session.get(url, headers=headers)
+            self.__save_file(path, number, response)
         except Exception as e:
             print("Exception in download_url_to_file(): ", e)
 
     
-    def __save_file(self, path, response):
+    def __save_file(self, path: str, number: int, response: Response):
+        path = self.strip_file_path(path)
+        pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+
+        file_path = os.path.join(path, "%03d" % (number) + ".jpg")
+
         try:
-            with open(path, 'wb') as f:
+            with open(file_path, 'wb') as f:
                 for chunk in response.iter_content(chunk_size=4096):
                     f.write(chunk)
         except Exception as e:
             print("Exception in __save_file(): ", e)
     
+
+    def strip_file_path(self, path):
+        """
+        윈도우에서 사용이 가능한 파일 명으로 변경
+        """
+        path = re.sub(r"NEW\t+", "", path)
+
+        path = path.replace('\n', '')
+        path = re.sub(r"\t.*$", "", path)
+        path = path.replace(':', '').replace('?', '').replace(
+            '/', '').replace('!', '').replace('\\', '')
+        path = path.replace("「", " ").replace("」", '').replace(".", "")
+        path = path.replace("<", "").replace(">", "")
+
+        path = path.strip()
+        return path
+
+
+    def zip_folder(self, filename, path):
+        """
+        폴더 압축하기
+        """
+        zipf = zipfile.ZipFile(filename, 'w', zipfile.ZIP_DEFLATED)
+        for f in os.listdir(path):
+            zipf.write(os.path.join(path, f), os.path.basename(f))
+        zipf.close()
