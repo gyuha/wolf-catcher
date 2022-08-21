a5368744c79858d6174424375de2d1d3e0044728
gyuha <nicegyuha@gmail.com>
2022-08-21 11:13:56 +0900

    update

--- src/util/Downloader.py
+++ src/util/Downloader.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import os
 import re
 import requests
@@ -9,28 +10,47 @@ from multiprocessing import cpu_count
 import pathlib
 
 
+class DOWNLOAD_TYPE(Enum):
+    THUMBNAIL = 1
+    IMAGES = 2
+
+
+class DOWNLOAD_TYPE(Enum):
+    READY = 0
+    DOING = 1
+    DONE = 2
+
+
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36"
 }
 
+
 class DownloaderSignal(QObject):
-    download_state = Signal(int)
+    download_state = Signal(int, DOWNLOAD_TYPE, int)
     download_error = Signal(str)
+
 
 class Downloader:
     signals = DownloaderSignal()
 
-    def __init__(self):
+    def __init__(self, referer: str):
         self.files = []
         self.downloading = False
         self.send_count = 0
+        self.referer
+    
+    def add_thumbnail(self, path: str, url: str) -> None:
+        self.type = DOWNLOAD_TYPE.THUMBNAIL
+        self.files = []
+        self.files.append([url, path, re])
+        pass
 
-
-    def add_files(self, path: str, urls: list, referer):
-        self.files = [];
+    def add_image_files(self, type: DOWNLOAD_TYPE, path: str, urls: list, referer) -> None:
+        self.type = DOWNLOAD_TYPE.IMAGES
+        self.files = []
         for url in urls:
             self.files.append([url, path, referer, len(self.files) + 1])
-    
 
     def download_run(self) -> None:
         self.downloading = True
@@ -40,13 +60,12 @@ class Downloader:
         try:
             results = pool.imap_unordered(self.__download_url_to_file, self.files)
             for result in results:
-                print('url:', result)
+                print("url:", result)
         finally:
             pool.close()
             pool.join()
             self.downloading = False
-            print('📢[Downloader.py:49]: ', self.downloading)
-
+            print("📢[Downloader.py:49]: ", self.downloading)
 
     # @retry(exceptions=Exception, tries=5, delay=0)
     def __download_url_to_file(self, args: list) -> None:
@@ -56,7 +75,7 @@ class Downloader:
 
         if referer:
             headers["Referer"] = referer
-        
+
         requests.urllib3.disable_warnings()
         session = requests.Session()
         session.headers.update(headers)
@@ -65,30 +84,27 @@ class Downloader:
             response = session.get(url, stream=True, verify=False)
 
             if response.status_code > 200:
-                raise Exception("Response error : {}"%(response.code))
+                raise Exception("Response error : {}" % (response.code))
 
             self.__save_file(path, number, response)
             self.__download_state_emit()
         except Exception as e:
             print("Exception in download_url_to_file(): ", e)
-        
+
         return number
-    
+
     def __download_state_emit(self):
         self.send_count = self.send_count + 1
-        self.signals.download_state.emit(self.send_count)
+        self.signals.download_state.emit(self.id, self.send_count)
 
-
-    
     def __save_file(self, path: str, number: int, response: Response):
         pathlib.Path(path).mkdir(parents=True, exist_ok=True)
 
         file_path = os.path.join(path, "%03d" % (number) + ".jpg")
 
         try:
-            with open(file_path, 'wb') as f:
+            with open(file_path, "wb") as f:
                 for chunk in response.iter_content(chunk_size=4096):
                     f.write(chunk)
         except Exception as e:
             print("Exception in __save_file(): ", e)
-    
